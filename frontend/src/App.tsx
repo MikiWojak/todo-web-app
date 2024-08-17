3355e63f5705455b5018d710f3bcbcd7493164f4
MikiWojak <mikizarowa8@gmail.com>
2024-08-17 09:07:51 +0200

    Install Tailwind CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const App: FC = () => {
     return (
-        <div>
+        <div className="w-screen h-screen bg-slate-800 text-white">
             <h1>TODO Web App</h1>
         </div>
     );

3229ad277175806945f8bfaf0d0f017af26ec3ac
MikiWojak <mikizarowa8@gmail.com>
2024-08-17 10:40:38 +0200

    Header WIP

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,7 +1,17 @@
+import { ArrowRightEndOnRectangleIcon } from '@heroicons/react/24/solid';
+
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className="flex w-100 h-16 p-4 bg-red-600 text-white">
+            <h1>TODO</h1>
+
+            <div>
+                Todos
+            </div>
+
+            <div>
+                <ArrowRightEndOnRectangleIcon className="size-6" />
+            </div>
         </div>
     );
 };

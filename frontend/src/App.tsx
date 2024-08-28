578367b428a7cadcd9e7179b9cb3c58cdfca5b57
MikiWojak <mikizarowa8@gmail.com>
2024-08-28 17:20:14 +0200

    Alias src

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,10 @@ import {
     createRoutesFromElements
 } from 'react-router-dom';
 
-import Login from './views/pages/Login';
-import Todos from './views/pages/Todos';
-import Profile from './views/pages/Profile';
-import Layout from './views/layouts/Layout';
+import Login from 'src/views/pages/Login';
+import Todos from 'src/views/pages/Todos';
+import Profile from 'src/views/pages/Profile';
+import Layout from 'src/views/layouts/Layout';
 
 const router = createBrowserRouter(
     createRoutesFromElements(

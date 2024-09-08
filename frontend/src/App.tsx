4357ed0ba2774e4ca65cad39c215241a4f2b06ec
Mikołaj Żarnowski <53621297+MikiWojak@users.noreply.github.com>
2024-09-08 08:15:30 +0200

    Absolute imports (#3)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,10 @@ import {
     createRoutesFromElements
 } from 'react-router-dom';
 
-import Login from './views/pages/Login';
-import Todos from './views/pages/Todos';
-import Profile from './views/pages/Profile';
-import Layout from './views/layouts/Layout';
+import Login from '@/views/pages/Login';
+import Todos from '@/views/pages/Todos';
+import Profile from '@/views/pages/Profile';
+import Layout from '@/views/layouts/Layout';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
